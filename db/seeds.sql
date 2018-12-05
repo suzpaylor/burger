b2a2ec56203ced7fb857915e4bc80780091ec03e
The Lizard <suzpaylor@gmail.com>
2018-12-04 18:28:48 -0700

    add data

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Crownburger", false, "2018-12-04 18:26:00"),
+  ("Whooper", false, "2018-12-04 18:26:01"),
+  ("McRib", false, "2018-12-04 18:26:02"),
+  ("Veggieburger", false, "2018-12-04 18:26:03");
