9fd113f4c7de759a49ed066b1b8ef2fcb80ce05e
The Lizard <suzpaylor@gmail.com>
2018-12-05 14:16:36 -0700

    heroku problems

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the 'public' directory
-app.use(express.static(process.cwd() + '/public'));
-
+//app.use(express.static(process.cwd() + '/public'));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
