40aa77b8be81d3ca47ceb99ea1bf9ef9847e149e
The Lizard <suzpaylor@gmail.com>
2018-12-05 13:02:50 -0700

    started over :/

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,7 @@
-INSERT INTO burgers(burger_name, devoured, date)
-VALUES ("Crownburger", false, "2018-12-04 18:26:00"),
-  ("Whooper", false, "2018-12-04 18:26:01"),
-  ("McRib", false, "2018-12-04 18:26:02"),
-  ("Veggieburger", false, "2018-12-04 18:26:03");
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Lime Burger', true);
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+// Pull in required dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 3000;
 
 var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
-// app.use(express.static('public'));
 
-// Parse application/x-www-form-urlencoded
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Handlebars
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the view engine
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Import routes and give the server access to them
+var routes = require('./controllers/burgers_controller.js');
 
-var router = require('./controllers/burgers_controllers.js');
-app.use('/', router);
+app.use('/', routes);
 
-// Open Server
-var port = process.env.PORT || 3000;
 app.listen(port);
